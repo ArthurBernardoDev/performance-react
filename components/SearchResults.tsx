f1dacae6d2392e87c47960230150a4eac30f5da9
ArthurBernardoDev <arthurutschgg@gmail.com>
2021-09-23 15:49:12 -0300

    useMemo

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ProductItem } from "./ProductItem";
+import { useMemo } from "react";
 
 interface SearchResultsProps {
   results: Array<{
@@ -10,8 +11,16 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }, [results]);
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
+
       {results.map((product) => {
         return <ProductItem product={product} />;
       })}
@@ -22,4 +31,18 @@ export function SearchResults({ results }: SearchResultsProps) {
  * 1 - Criar uma nova versão do component
  * 2 - Comparar com a versão anterior
  * 3 - Se houverem alterações, vai atualizar o que alterou
+ */
+
+/**
+ * 1- Pure Functional Components
+ * 2- Renders too often
+ * 3- Re-renders with same props
+ * 4- Medium to big size
+ */
+
+/**
+ * useMemo / useCallback
+ *
+ * 1- Cálculos pesados
+ * 2- Igualdade referencial (quando se repassa uma informação a um component filho)
  */
